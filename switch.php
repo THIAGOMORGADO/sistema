7eaed718d17a94d29f363afdbe4feaa3ae84377f
morgado <thiago_morgado@hotmail.com>
2018-01-16 04:42:56 -0200

    esse e um switch

---
+++ switch.php
@@ -0,0 +1,19 @@
+<?php
+$x = 0;
+
+switch ($x)
+{
+    case 0:
+    case 1:
+    case 2:
+        echo "aqui pode esta um valor de 0 a 2";
+        break;
+
+
+    default:
+        echo "aqui nao e nada";
+        break;
+}
+
+
+?>
